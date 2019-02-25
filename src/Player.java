1014563fcbbcc30f6883e66c35af50e17854d11f
xinweiiiii <43470166+xinweiiiii@users.noreply.github.com>
2019-02-25 22:18:56 +0800

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,26 +1,26 @@
 public class Player {
 
-  private boolean isComputer;
-  private int pointsFromCurrentRound = 0;
-  private int totalPoints = 0;
+    private boolean isComputer;
+    private int pointsFromCurrentRound = 0;
+    private int totalPoints = 0;
 
-  public Player(boolean isComputer) {
-    this.isComputer = isComputer;
-  }
+    public Player(boolean isComputer) {
+        this.isComputer = isComputer;
+    }
 
-  public int getPointsFromCurrentRound() {
-    return pointsFromCurrentRound;
-  }
+    public int getPointsFromCurrentRound() {
+        return pointsFromCurrentRound;
+    }
 
-  public void setPointsFromCurrentRound(int pointsFromCurrentRound) {
-    this.pointsFromCurrentRound = pointsFromCurrentRound;
-  }
+    public void setPointsFromCurrentRound(int pointsFromCurrentRound) {
+        this.pointsFromCurrentRound = pointsFromCurrentRound;
+    }
 
-  public int getTotalPoints() {
-    return totalPoints;
-  }
+    public int getTotalPoints() {
+        return totalPoints;
+    }
 
-  public void setTotalPoints(int totalPoints) {
-    this.totalPoints = totalPoints;
-  }
+    public void setTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
 }

b19c793b2b51f39c6285baa87002d217853bb891
shockershock <michael-tjc@hotmail.com>
2019-02-25 22:27:10 +0800

    Added a Test.java

--- src/Round.java
+++ src/Round.java
@@ -1,35 +1,35 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Round{
-    private ArrayList<Player>listOfPlayers = new ArrayList<>();
-    private int setNo;
-    private boolean heartsBroken;
-    private int roundNo;
-    private int leadingPlayer;
+public class Round {
+  private static ArrayList<Player> listOfPlayers;
+  private boolean heartsBroken;
+  private int setNo;
+  private int roundNo;
+  private int leadingPlayer;
 
-    private void Round(){
-        if (setNo == 13){
-            roundNo++;
-            setNo = 0;
-        }
-    }
+  public Round() {
+    listOfPlayers = new ArrayList<>();
+    listOfPlayers.add(new Player(false));
+    listOfPlayers.add(new Player(true));
+    listOfPlayers.add(new Player(true));
+    listOfPlayers.add(new Player(true));
+  }
 
-    public void startRound(){
+  public void startRound() {
 
-    }
+  }
 
-    public void HumanexchangeCards(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("");
-
-
-    }
-    public void tallyPoints(int points, Player player) {
-
-    }
+  public void HumanExchangeCards() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("");
 
 
+  }
 
+  public void tallyPoints(int points, Player player) {
+
+  }
 
 
 }

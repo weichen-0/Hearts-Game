1286fd6a21250a06ad72879ddf79a96a1ccc01d7
shockershock <michael-tjc@hotmail.com>
2019-04-08 13:27:23 +0800

    Player.java missing semi colon

--- src/Player.java
+++ src/Player.java
@@ -1,48 +1,54 @@
 public class Player {
 
-  private String name;
-  private boolean isPlayer;
-  private int pointsFromCurrentRound = 0;
-  private int totalPoints = 0;
-  private Hand hand = new Hand();
+    private String name;
+    private boolean isPlayer;
+    private int pointsFromCurrentRound = 0;
+    private int totalPoints = 0;
+    private Hand hand = new Hand();
 
-  public Player(String name, boolean isPlayer) {
-    this.isPlayer = isPlayer;
-    this.name = name;
-  }
+    public Player(String name, boolean isPlayer) {
+        this.isPlayer = isPlayer;
+        this.name = name;
+    }
 
-  public String getName() {
-      return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public boolean getIsPlayer(){
-    return isPlayer;
-  }
+    public boolean getIsPlayer() {
+        return isPlayer;
+    }
 
-  public int getPointsFromCurrentRound() { return pointsFromCurrentRound }
+    public int getPointsFromCurrentRound() {
+        return pointsFromCurrentRound;
+    }
 
-  public void resetPointsFromCurrentRound() {
-    pointsFromCurrentRound = 0;
-  }
+    public void resetPointsFromCurrentRound() {
+        pointsFromCurrentRound = 0;
+    }
 
-  public void addToPointsFromCurrentRound(int points) { // points from set
-    this.pointsFromCurrentRound += points;
-  }
+    public void addToPointsFromCurrentRound(int points) { // points from set
+        this.pointsFromCurrentRound += points;
+    }
 
-  public int getTotalPoints() {
-    return totalPoints;
-  }
+    public int getTotalPoints() {
+        return totalPoints;
+    }
 
-  public void addToTotalPoints(int points) { // points from round
-    this.totalPoints += points;
-  }
+    public void addToTotalPoints(int points) { // points from round
+        this.totalPoints += points;
+    }
 
-  public void tallyTotalPoints(int totalPoints) {
-    this.totalPoints = totalPoints;
-  }
+    public void tallyTotalPoints(int totalPoints) {
+        this.totalPoints = totalPoints;
+    }
 
-  public Hand getHand() { return hand; }
+    public Hand getHand() {
+        return hand;
+    }
 
-  public int getHandSize() { return hand.getNumberOfCards(); }
+    public int getHandSize() {
+        return hand.getNumberOfCards();
+    }
 
 }

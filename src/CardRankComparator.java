280c87eb9a1083b0c93a0f6dc60ea28cc94af2b2
weichen <behweichen@gmail.com>
2019-04-09 20:29:19 +0800

    update

--- src/CardRankComparator.java
+++ src/CardRankComparator.java
@@ -1,6 +1,19 @@
 import java.util.Comparator;
 
+/**
+ *  Class that compares cards by their rank followed by their suit.
+ */
 public class CardRankComparator implements Comparator<Card> {
+
+    /**
+     * Compares 2 Card objects to see if first Card is less than, equal to,
+     * or greater than the second Card based on rank followed by suit.
+     *
+     * @param card1 first Card object
+     * @param card2 second Card object
+     * @return rankDiff integer if Cards are of different rank
+     * else suitDiff integer if Cards are of same rank
+     */
     @Override
     public int compare(Card card1, Card card2) {
         int suitDiff = card1.getSuit().compareTo(card2.getSuit());
--- src/CardSuitComparator.java
+++ src/CardSuitComparator.java
@@ -1,6 +1,18 @@
 import java.util.Comparator;
 
+/**
+ *  Class that compares cards by their suit followed by their rank.
+ */
 public class CardSuitComparator implements Comparator<Card> {
+    /**
+     * Compares 2 Card objects to see if first Card is less than, equal to,
+     * or greater than the second Card based on suit followed by rank.
+     *
+     * @param card1 first Card object
+     * @param card2 second Card object
+     * @return suitDiff integer if Cards are of different suit
+     * else rankDiff integer if Cards are of same suit
+     */
     @Override
     public int compare(Card card1, Card card2) {
         int suitDiff = card1.getSuit().compareTo(card2.getSuit());
